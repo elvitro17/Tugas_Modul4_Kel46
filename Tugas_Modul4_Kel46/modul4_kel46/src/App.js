05ac0c2c9eb72fe51b33138260564daa055ef2e5
DafiRamadhan <48998412+DafiRamadhan@users.noreply.github.com>
2021-09-19 11:05:57 +0700

    Update App.js

--- Tugas_Modul4_Kel46/modul4_kel46/src/App.js
+++ Tugas_Modul4_Kel46/modul4_kel46/src/App.js
@@ -2,15 +2,24 @@ import React, {useState, useMemo, useCallback} from 'react';
 
 function App() {
        
-    const [fib, set_fib] = useState(3);
+       const [name, set_name] = useState(' ');
+       const [nim, set_nim] = useState(' ');
+       const [fib, set_fib] = useState(3);
       
-    const on_fib_changed = useCallback((e) => set_fib(Number(e.target.value)), []);
+       const on_name_changed = useCallback((e) => set_name(e.target.value), []);
+       const on_nim_changed = useCallback((e) => set_nim(e.target.value), []);
+       const on_fib_changed = useCallback((e) => set_fib(Number(e.target.value)), []);
    
   
 
   return (
     <>
-       
+        <input value={name} onChange={on_name_changed} />
+        <NameDisplay name={name} />
+        <input value={nim} onChange={on_nim_changed} />
+        <NimDisplay nim={nim} />
+      <hr />
+
           <input value={fib} onChange={on_fib_changed} />
 
           <FibDisplay fib={fib} />
@@ -31,6 +40,15 @@ function FibDisplay({fib}) {
     return <p>{fib} jumlah bilangan  fibonacci: {numbers.join(', ')}</p>;
 }
 
+const NameDisplay = React.memo(function ({name}) {
+  console.log('Nama Diubah');
+  return <p>Nama = {name}</p>;
+});
+
+const NimDisplay = React.memo(function ({ nim }) {
+    console.log('Nim Diubah');
+    return <p>NIM = {nim}</p>;
+});
 
 
 
